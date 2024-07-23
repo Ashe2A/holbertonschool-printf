593945229290b899baa18989b12b5b0c3e904168
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-23 12:00:04 +0200

    0 main.h

--- main.h
+++ main.h
@@ -1,8 +1,23 @@
-#ifndef PRINTF_FUNCTIONS
-#define PRINTF_FUNCTIONS
+#ifndef PRINTPUT_FUNCTIONS
+#define PRINTPUT_FUNCTIONS
 
-int print_char(va_list l);
-int print_string(va_list l);
+int _printf(const char *string, ...);
+
+int print_char(va_list arg_list);
+int print_string(va_list arg_list);
 int print_percent();
+
+int _putchar(char character);
+
+#endif
+
+#ifndef PERCENT
+#define PERCENT
+
+typedef structure percent_indicator
+{
+    char percent_type;
+	int (*print_function)(va_list arg_list);
+} pct_t;
 
 #endif
