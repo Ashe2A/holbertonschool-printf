058f3730aaaa9980a389181a7e9175181e6d165e
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-25 11:00:30 +0200

    0 header betty A

--- main.h
+++ main.h
@@ -3,14 +3,14 @@
 #include <stdarg.h>
 
 /**
- * struct percent_indicator - Percent indicator 
+ * struct percent_indicator - Percent indicator
  *
  * @percent_type: The character following the percent sign
- * @f: The function associated with the character
+ * @print_function: The function associated with the character
  *
  * Description: Find which function to call
-depending on the character following a percent sign
-(c for character and s for string)
+ * depending on the character following a percent sign
+ * (c for character and s for string)
  */
 typedef struct percent_indicator
 {
@@ -22,7 +22,7 @@ int _printf(const char *, ...);
 
 int print_char(va_list);
 int print_string(va_list);
-int print_percent();
+int print_percent(void);
 
 int _putchar(char);
 
