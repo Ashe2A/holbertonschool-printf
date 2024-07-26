263952d28bbb86d4b70a822c7412432b1b5755f7
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-26 23:47:07 +0200

    1 correct tests

--- main.c
+++ main.c
@@ -13,38 +13,38 @@ int main(void)
 
 	len = _printf("8566644");
 	len2 = printf("8566644");
-	printf("NULL length (_printf):[%d, %i]\n", len, len);
-	_printf("NULL length (_printf):[%d, %i]\n", len, len);
-	printf("NULL length (printf):[%d, %i]\n", len2, len2);
-	_printf("NULL length (printf):[%d, %i]\n", len2, len2);
+	printf("Long positive int (_printf):[%d, %i]\n", len, len);
+	_printf("Long positive int (_printf):[%d, %i]\n", len, len);
+	printf("Long positive int (printf):[%d, %i]\n", len2, len2);
+	_printf("Long positive int (printf):[%d, %i]\n", len2, len2);
 
 	len = _printf("5");
 	len2 = printf("5");
-	printf("Percent length (_printf):[%d, %i]\n", len, len);
-	_printf("Percent length (_printf):[%d, %i]\n", len, len);
-	printf("Percent length (printf):[%d, %i]\n", len2, len2);
-	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	printf("Short positive int (_printf):[%d, %i]\n", len, len);
+	_printf("Short positive int (_printf):[%d, %i]\n", len, len);
+	printf("Short positive int (printf):[%d, %i]\n", len2, len2);
+	_printf("Short positive int (printf):[%d, %i]\n", len2, len2);
 
 	len = _printf("0");
 	len2 = printf("0");
-	printf("Percent length (_printf):[%d, %i]\n", len, len);
-	_printf("Percent length (_printf):[%d, %i]\n", len, len);
-	printf("Percent length (printf):[%d, %i]\n", len2, len2);
-	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	printf("Zero (_printf):[%d, %i]\n", len, len);
+	_printf("Zero (_printf):[%d, %i]\n", len, len);
+	printf("Zero (printf):[%d, %i]\n", len2, len2);
+	_printf("Zero (printf):[%d, %i]\n", len2, len2);
 
 	len = _printf("-681");
 	len2 = printf("-681");
-	printf("Percent length (_printf):[%d, %i]\n", len, len);
-	_printf("Percent length (_printf):[%d, %i]\n", len, len);
-	printf("Percent length (printf):[%d, %i]\n", len2, len2);
-	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	printf("Long negative int (_printf):[%d, %i]\n", len, len);
+	_printf("Long negative int (_printf):[%d, %i]\n", len, len);
+	printf("Long negative int (printf):[%d, %i]\n", len2, len2);
+	_printf("Long negative int (printf):[%d, %i]\n", len2, len2);
 
 	len = _printf("-4");
 	len2 = printf("-4");
-	printf("Percent length (_printf):[%d, %i]\n", len, len);
-	_printf("Percent length (_printf):[%d, %i]\n", len, len);
-	printf("Percent length (printf):[%d, %i]\n", len2, len2);
-	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	printf("Short negative int (_printf):[%d, %i]\n", len, len);
+	_printf("Short negative int (_printf):[%d, %i]\n", len, len);
+	printf("Short negative int (printf):[%d, %i]\n", len2, len2);
+	_printf("Short negative int (printf):[%d, %i]\n", len2, len2);
 
 	return (0);
 }
