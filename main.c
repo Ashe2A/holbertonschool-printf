335933c8146f04477619fc2ad90378db77893e4a
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-26 23:51:41 +0200

    1 better tests

--- main.c
+++ main.c
@@ -11,36 +11,36 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("8566644");
-	len2 = printf("8566644");
+	len = _printf("LPI %d et %i", 8566644, 8566644);
+	len2 = printf("LPI %d et %i", 8566644, 8566644);
 	printf("Long positive int (_printf):[%d, %i]\n", len, len);
 	_printf("Long positive int (_printf):[%d, %i]\n", len, len);
 	printf("Long positive int (printf):[%d, %i]\n", len2, len2);
 	_printf("Long positive int (printf):[%d, %i]\n", len2, len2);
 
-	len = _printf("5");
-	len2 = printf("5");
+	len = _printf("SPI %d et %i", 5, 5);
+	len2 = printf("SPI %d et %i", 5, 5);
 	printf("Short positive int (_printf):[%d, %i]\n", len, len);
 	_printf("Short positive int (_printf):[%d, %i]\n", len, len);
 	printf("Short positive int (printf):[%d, %i]\n", len2, len2);
 	_printf("Short positive int (printf):[%d, %i]\n", len2, len2);
 
-	len = _printf("0");
-	len2 = printf("0");
+	len = _printf("Z %d et %i", 0, 0);
+	len2 = printf("Z %d et %i", 0, 0);
 	printf("Zero (_printf):[%d, %i]\n", len, len);
 	_printf("Zero (_printf):[%d, %i]\n", len, len);
 	printf("Zero (printf):[%d, %i]\n", len2, len2);
 	_printf("Zero (printf):[%d, %i]\n", len2, len2);
 
-	len = _printf("-681");
-	len2 = printf("-681");
+	len = _printf("LNI %d et %i", -681, -681);
+	len2 = printf("LNI %d et %i", -681, -681);
 	printf("Long negative int (_printf):[%d, %i]\n", len, len);
 	_printf("Long negative int (_printf):[%d, %i]\n", len, len);
 	printf("Long negative int (printf):[%d, %i]\n", len2, len2);
 	_printf("Long negative int (printf):[%d, %i]\n", len2, len2);
 
-	len = _printf("-4");
-	len2 = printf("-4");
+	len = _printf("SNI %d et %i", -4, -4);
+	len2 = printf("SNI %d et %i", -4, -4);
 	printf("Short negative int (_printf):[%d, %i]\n", len, len);
 	_printf("Short negative int (_printf):[%d, %i]\n", len, len);
 	printf("Short negative int (printf):[%d, %i]\n", len2, len2);
