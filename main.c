e470e8aac8d73d5157fd709b83e26b89058e6917
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-26 11:30:26 +0200

    0 - lone percent test

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("Print a %%simple% sentence (%c, %s).\n", 'c', "String");
+	len = _printf("_Print a %%simple% sentence (%c, %s).\n", 'c', "String");
 	len2 = printf("Print a %%simple% sentence (%c, %s).\n", 'c', "String");
 	printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
@@ -20,5 +20,10 @@ int main(void)
 	len2 = printf(NULL);
 	printf("NULL Length (printf):[%d, %i]\n", len, len);
 	printf("NULL Length (_printf):[%d, %i]\n", len2, len2);
+
+	len = _printf("%");
+	len2 = printf("%");
+	printf("Percent Length (printf):[%d, %i]\n", len, len);
+	printf("Percent Length (_printf):[%d, %i]\n", len2, len2);
 	return (0);
 }
