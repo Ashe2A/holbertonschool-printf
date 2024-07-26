f81078729d6759c6c54b47b4a3b53947c17a9c61
JayDAshe <36708012+Ashe2A@users.noreply.github.com>
2024-07-26 23:43:42 +0200

    1 - all tests

--- main.c
+++ main.c
@@ -11,19 +11,40 @@ int main(void)
 {
 	int len, len2;
 
-	/* len = _printf("_Print a %%simple% sentence (%c, %s).\n", 'c', "String");
-	len2 = printf("Print a %%simple% sentence (%c, %s).\n", 'c', "String");
-	printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2); */
-
-	len = _printf(NULL);
-	len2 = printf(NULL);
+	len = _printf("8566644");
+	len2 = printf("8566644");
 	printf("NULL length (_printf):[%d, %i]\n", len, len);
+	_printf("NULL length (_printf):[%d, %i]\n", len, len);
 	printf("NULL length (printf):[%d, %i]\n", len2, len2);
+	_printf("NULL length (printf):[%d, %i]\n", len2, len2);
 
-	len = _printf("%");
-	len2 = printf("%");
+	len = _printf("5");
+	len2 = printf("5");
 	printf("Percent length (_printf):[%d, %i]\n", len, len);
+	_printf("Percent length (_printf):[%d, %i]\n", len, len);
 	printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+
+	len = _printf("0");
+	len2 = printf("0");
+	printf("Percent length (_printf):[%d, %i]\n", len, len);
+	_printf("Percent length (_printf):[%d, %i]\n", len, len);
+	printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+
+	len = _printf("-681");
+	len2 = printf("-681");
+	printf("Percent length (_printf):[%d, %i]\n", len, len);
+	_printf("Percent length (_printf):[%d, %i]\n", len, len);
+	printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+
+	len = _printf("-4");
+	len2 = printf("-4");
+	printf("Percent length (_printf):[%d, %i]\n", len, len);
+	_printf("Percent length (_printf):[%d, %i]\n", len, len);
+	printf("Percent length (printf):[%d, %i]\n", len2, len2);
+	_printf("Percent length (printf):[%d, %i]\n", len2, len2);
+
 	return (0);
 }
