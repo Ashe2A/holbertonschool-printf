00c12d9cd718c123031616fee85640d92201f0f1
Achraf Jeddi <36708012+Ashe2A@users.noreply.github.com>
2024-08-19 09:57:03 +0200

    1 - correcting tests

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("_Print a %%simple% sentence (%c, %s).\n", 'c', "String");
-	len2 = printf("Print a %%simple% sentence (%c, %s).\n", 'c', "String");
+	len = _printf("_Print a %%simple sentence (%c, %s, %d, %i).\n", 'c', "String", 444, 19);
+	len2 = printf("Print a %%simple sentence (%c, %s, %d, i).\n", 'c', "String", 444, 19);
 	printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 
